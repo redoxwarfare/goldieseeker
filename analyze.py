19a8c245fee9f8b40b85f21fb2cdea01c0ed771a
redox <redoxwarfare@gmail.com>
2020-08-08 17:16:43 -0400

    add timing tests; add plotting flag to analyze.py

--- analyze.py
+++ analyze.py
@@ -20,6 +20,7 @@ parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
                     help='Show this help message.')
 parser.add_argument("map_id", nargs='*', help="name(s) of .txt file(s) in gusher graphs folder for map(s) to analyze")
 parser.add_argument("-l", "--log", help="print log of search algorithm's internal process", action='store_true')
+parser.add_argument("-p", "--plot", help="display gusher graph", action='store_true')
 
 
 def load_graph(mapname):  # TODO - separate gusher map and penalty assignment(s) into 2 files
@@ -69,7 +70,7 @@ mbhybrid = readtree('b(e(d, c(a,)), c*(f, h(g,)))', load_graph('mb'))
 lostaysee = readtree('h(f(e, g(i,)), f*(d, a(c(b,),)))', load_graph('lo'))
 
 
-def report(map_id, log=False):
+def report(map_id, log=False, plot=False):
     G = load_graph(map_id)
     print(f'\nMap: {G.graph["name"]}')
 
@@ -99,10 +100,11 @@ def report(map_id, log=False):
                   f'    costs: {{' + ', '.join(f'{node}: {costs[node]}' for node in costs) + '}\n' +
                   f'    mean cost: {mean(costs.values()):0.2f}, stdev: {pstdev(costs.values()):0.2f}')
 
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        print('(may need to close graph plot to continue)')
-        plot_graph(G)
+    if plot:
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            print('(may need to close graph plot to continue)')
+            plot_graph(G)
 
 
 def main():
@@ -111,11 +113,11 @@ def main():
     while not stop:
         try:
             for map_id in args.map_id:
-                report(map_id, args.log)
+                report(map_id, args.log, args.plot)
         except FileNotFoundError as e:
             print(f"Couldn't find {e.filename}!")
 
-        input_str = input('Input map ID(s) or "quit" to quit: ')
+        input_str = input('Input map ID(s), "-h" for help, or "quit" to quit: ')
         if input_str == "quit":
             stop = True
         else:
