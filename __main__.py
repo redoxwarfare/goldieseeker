c1085d99408f51bcdfc345e9c6a06f8f7d05e34e
redox <redoxwarfare@gmail.com>
2020-08-30 19:20:19 -0400

    fix CLI help message formatting

--- __main__.py
+++ __main__.py
@@ -11,22 +11,25 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument('map_id')
-@click.option('--weights', '-W', type=str,
-              help="""dictionary for specifying custom gusher weights
-              \b\nexample: -W \"{'d': 4, 'bef': 2, '.': 1}\"
-              \b\nthis gives a weight of 4 to gusher D, a weight of 2 to gushers B, E, and F, """
-              "and a weight of 1 to the rest")
 @click.option('--tuning', '-t', type=click.FloatRange(0, 1), default=0.5,
-              help="""set seeking algorithm tuning factor (between 0-1)
-              \b\n0: fastest time, 1: lowest risk""")
+              help="""\b
+              Set the seeking algorithm's tuning factor (range between 0-1).
+              0: fastest time, 1: lowest risk""")
 @click.option('--eval', '-E', 'strategy_str', type=str,
-              help="""evaluate a user-specified strategy
-              \b\nexample: -E \"f(d(b, g), e(c, a))\"
-              \b\n"a(b, c)" means if a is high, open b, and if a is low, open c""")
+              help="""\b
+              Evaluate a user-specified strategy.
+              example: -E "f(d(b, g), e(c, a))"
+              "a(b, c)" means "if a is high, open b, and if a is low, open c\"""")
+@click.option('--weights', '-W', type=str,
+              help="""\b
+              Specify custom gusher weights in dictionary format.
+              example: -W "{'d': 4, 'bef': 2, '.': 1}"
+              This gives a weight of 4 to gusher D, a weight of 2 to gushers B, E, and F, """
+              "and a weight of 1 to the rest.")
 @click.option('--suppress', '-s', is_flag=True,
-              help="don't show plot of the map")
+              help="Don't show the plot of the map.")
 @click.option('--debug', '-d', is_flag=True,
-              help="print internal process of search algorithm")
+              help="Print internal process of search algorithm.")
 def main(map_id, weights, tuning, strategy_str, suppress, debug):
     """For a given map, generate a Goldie Seeking strategy or evaluate a user-specified strategy."""
     gusher_map = GusherMap(map_id, weights=weights)
