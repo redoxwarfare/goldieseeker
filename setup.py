1d9e37ee32c43e87ba065f052ef525ddfa1e8d86
redox <redoxwarfare@gmail.com>
2020-08-31 01:25:42 -0400

    trying to set up package correctly for setuptools

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
         author='redoxwarfare',
         url='https://github.com/redoxwarfare/goldieseeker',
         packages=find_packages(),
+        package_data={'goldieseeker': ['maps/*.csv', 'maps/*.txt', 'images/*.png']},
         install_requires=[
                 'click',
                 'networkx',
@@ -15,10 +16,11 @@ setup(
                 'scipy',
                 'pyparsing'
         ],
-        entry_points="""
-            [console_scripts]
-            gseek=goldieseeker:cli
-        """,
+        entry_points={
+            'console_scripts': [
+                    'gseek = goldieseeker.__main__:main'
+            ]
+        },
         classifiers=[
                 'Programming Language :: Python :: 3',
                 'License :: OSI Approved :: MIT License',
